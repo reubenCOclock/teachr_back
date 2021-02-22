0d4cf822cd0bdc5dbd7a38edf36312f8f971cdb2
yunities-front <chourakirgit@yahoo.com>
2021-02-22 19:18:15 +0100

    add teachr controller

---
+++ src/Controller/TeachrController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+
+?>
